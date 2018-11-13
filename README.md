4b37e67ceaf4b6c978d84020fea40eaa9c6e3587
yasutakaono <35129436+yasutakaono@users.noreply.github.com>
2018-11-13 19:51:45 +0900

    '最初のコミット'

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_stack_over_flow_api
+
+A new Flutter application.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  final url =
+      'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=flutter&site=stackoverflow';
+  final controller = StreamController<List<StackOverflowInfo>>();
+
+  @override
+  void initState() {
+    super.initState();
+    refreshQuestions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // TODO: (not all material) pull into above widget.
+        title: 'StackOverflow Viewer',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: Scaffold(
+            body: Container(
+              color: const Color(0xffc0c0c0),
+              child: Column(children: [
+                CustomAppBar('Stack Overflow TODO'),
+                StackOverflowContent(controller.stream),
+                PlatformButton(
+                    child: const Text('Refresh'),
+                    icon: const Icon(Icons.refresh),
+                    onPressed: refreshQuestions)
+              ]),
+            )));
+  }
+
+  void refreshQuestions() async {
+    var result = await http.get(url);
+    Map decoded = json.decode(result.body);
+    List items = decoded['items'];
+    controller.add(items
+        .where((item) => !item['is_answered'])
+        .map<StackOverflowInfo>((item) => StackOverflowInfo.fromJson(item))
+        .toList());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.close();
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  final String title;
+  CustomAppBar(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Container(
+      padding: EdgeInsets.only(top: statusBarHeight),
+      height: statusBarHeight * 4,
+      child: Center(
+        child: Text(
+          title,
+          style: const TextStyle(
+              color: Colors.white, fontFamily: 'Kranky', fontSize: 36.0),
+        ),
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepOrange,
+            Colors.orangeAccent,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StackOverflowInfo {
+  String title;
+  int viewCount;
+  StackOverflowInfo.fromJson(Map json) {
+    title = json['title'];
+    viewCount = json['view_count'];
+  }
+}
+
+class StackOverflowContent extends StatelessWidget {
+  final Stream<List<StackOverflowInfo>> questionStream;
+
+  StackOverflowContent(this.questionStream);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: questionStream,
+        builder: (BuildContext context,
+            AsyncSnapshot<List<StackOverflowInfo>> snapshot) {
+          if (snapshot.hasError)
+            return Text('Error ${snapshot.error}');
+          else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Receiving questions...');
+          }
+          return Expanded(
+              child: ListView(
+                  children: snapshot.data
+                      .map<Widget>((info) => Card(
+                    child: ListTile(
+                        title: Text(info.title),
+                        leading: CircleAvatar(
+                          child: Text(info.viewCount.toString()),
+                        )),
+                  ))
+                      .toList()));
+        });
+  }
+}
+
+class PlatformButton extends StatelessWidget {
+  PlatformButton({Key key, this.child, this.icon, this.onPressed})
+      : super(key: key);
+  final Widget child;
+  final Widget icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      return CupertinoButton(
+        child: child,
+        onPressed: onPressed,
+      );
+    } else {
+      return FloatingActionButton(
+        child: icon,
+        onPressed: onPressed,
+      );
+    }
+  }
+}
